d1ca258780acba7195847dbeb63b474a6e97ccad
Naoki.Ii <naoki1230f@gmail.com>
2021-01-21 20:22:18 +0900

    add memo screen

---
+++ src/screens/MemoDetaillScreen.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoDetailScreen(){
+    return(
+        <View style={styles.container}>
+            <AppBar/>
+            <View style={styles.memoheader}>
+                <Text style={styles.memotitle}>買い物リスト</Text>
+                <Text style={styles.memodate}>2020/12/24 00:00:00</Text>
+            </View>
+            <ScrollView style={styles.memobody}>
+                <View style={styles.memotext}>
+                    <Text>
+                    こちらに本文が入ります
+                    </Text>
+                </View>
+            </ScrollView>
+            <CircleButton style={{top:160, bottom: 'auto'}}>＋</CircleButton>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#ffffff',
+    },
+
+    memoheader: {
+        height:96,
+        backgroundColor:'#6699FF',
+        justifyContent: 'center',
+        paddingVertical:24,
+        paddingHorizontal:19,
+
+    },
+    memotitle: {
+        color: '#ffffff',
+        fontSize:28,
+        lineHeight:32,
+        fontWeight:'bold',
+    },
+    memodate: {
+        color: '#ffffff',
+        fontSize: 12,
+        lineHeight: 16,
+    },
+    memobody: {
+        paddingVertical:32,
+        paddingHorizontal:27,
+    },
+    memotext: {
+        fontSize:16,
+        lineHeight:16,
+    },
+});
